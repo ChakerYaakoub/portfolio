605cde194454afca814fd56b82072b1a711c09a3
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-05-26 04:57:20 +0200

    try to create the data base 8

--- src/utils/dataCreate.js
+++ src/utils/dataCreate.js
@@ -1,8 +1,8 @@
 // Import the necessary Firebase modules
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, doc, setDoc } from 'firebase/firestore';
-import { firebaseConfig } from './serviceAccountKey.json'; // Import your Firebase configuration file
 import dataUser from './data.json'; // Import the JSON data to be added to Firestore
+import firebaseConfig from './serviceAccountKey';
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
